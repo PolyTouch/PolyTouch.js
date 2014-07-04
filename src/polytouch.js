4289a4bd56feb4fd1538c961cf2058ed7071e928
Damien Antipa <dantipa@adobe.com>
2014-07-04 11:24:08 +0200

    functional pointer map

--- src/polytouch.js
+++ src/polytouch.js
@@ -25,7 +25,9 @@
 
         _sanitizeEvent: function (event) {
             // todo sanitize data from events
-            return {};
+            return {
+                pointerId: event.pointerId
+            };
         },
 
         _add: function (event) {
@@ -34,49 +36,77 @@
             this.events.push(data);
 
             return data; // for the reference
-        }
+        },
+
+        add: function (event) {
+            switch (event.type) {
+                case 'pointerdown':
+                    this.start(event);
+                    break;
+                case 'pointermove':
+                    this.move(event);
+                    break;
+                case 'pointercancel':
+                case 'pointerup':
+                    this.end(event);
+                    break;
+            }
+        },
 
         start: function (event) {
-            return this.start = this._add(event);
+            return (this.start = this._add(event));
         },
 
         move: function (event) {
-            return this.lastMove = this._add(event);
+            return (this.lastMove = this._add(event));
         },
 
         end: function (event) {
-            return this.end = this._add(event);
+            return (this.end = this._add(event));
         },
     };
 
     function PointerMap() {
         this.keys = [];
-        this.values = [];
+        this.values = {};
     }
 
     PointerMap.prototype = {
 
         set: function (id, event) {
+            var idx = this.keys.indexOf(id);
 
+            if (idx === -1) {
+                this.keys.push(id);
+                this.values[id] = new Pointer(id);
+            }
+
+            this.values[id].add(event);
         },
 
         get: function (id) {
-
+            return this.values[id];
         },
 
         remove: function (id) {
-
+            var idx = this.keys.indexOf(id);
+            if (idx > -1) {
+                this.keys.splice(idx, 1);
+                delete this.values[id];
+            }
         },
 
         has: function (id) {
-
+            return this.keys.indexOf(id) > -1;
         },
 
         clear: function () {
+            for (var i=this.keys.length; i > -1; i--) {
+                delete this.values[this.keys[i]];
+            }
 
+            this.keys.length = 0;
         }
-
-
     };
 
     global.polyTouch = {
