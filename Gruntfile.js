0a5d6624dd681b4d4b0d37c85227153e640d8509
Damien Antipa <dantipa@adobe.com>
2014-07-04 10:19:54 +0200

    init

---
+++ Gruntfile.js
@@ -0,0 +1,64 @@
+module.exports = function(grunt) {
+
+    var pkg = require('./package.json'),
+        buildTime =  new Date();
+
+    grunt.initConfig({
+        outputName: 'polytouch',
+        replace: {
+            build: {
+                files: [{
+                    src: ['src/polytouch.js'],
+                    dest: '<%= outputName %>.js'
+                }],
+                options: {
+                    patterns: [{
+                        json: pkg
+                    }, {
+                        json: {
+                            date: buildTime.toISOString(),
+                            year: buildTime.getFullYear()
+                        }
+                    }]
+                }
+            }
+        },
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc',
+                reporter: require('jshint-stylish')
+            },
+            all: ['<%= outputName %>.js']
+        },
+        uglify: {
+            min: {
+                options: {
+                    preserveComments: 'some'
+                },
+                files : {
+                    '<%= outputName %>.min.js' : ['<%= outputName %>.js']
+                }
+            }
+        },
+        yuidoc : {
+            compile: {
+                name: pkg.name,
+                description: pkg.description,
+                version: pkg.version,
+                url: pkg.homepage,
+                options: {
+                    ignorePaths: ['src/', 'example', 'docs', 'node_modules'],
+                    paths: '.',
+                    outdir: 'docs/'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-replace');
+    grunt.loadNpmTasks('grunt-contrib-yuidoc');
+
+    grunt.registerTask('default', ['replace', 'jshint', 'uglify', 'yuidoc']);
+};
---
+++ README.md
@@ -0,0 +1,15 @@
+PolyTouch
+====================================
+
+
+
+Dependencies
+------------------------------------
+W3C Pointer Events support, either native or through a polyfill like [Google's Polymer polyfill](https://github.com/polymer/PointerEvents) or [Microsoft's Hand.js](http://handjs.codeplex.com/).
+
+
+Browser Support
+------------------------------------
+Without polyfill: IE11
+
+With polyfill: IE10+,  evergreens Chrome, Firefox, Android 3.0+, iOS 6.0+
