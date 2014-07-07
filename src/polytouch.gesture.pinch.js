44eb1ff576b7eba0e12de61bb38d31b974d1083b
Damien Antipa <dantipa@adobe.com>
2014-07-07 13:36:57 +0200

    add scale and rotation calculation

--- src/polytouch.gesture.pinch.js
+++ src/polytouch.gesture.pinch.js
@@ -11,6 +11,20 @@
  */
 (function (global) {
 
+    function scale(s, e) {
+        return (Math.sqrt(Math.pow(
+                e[0].x - e[1].x, 2) + Math.pow(e[0].y - e[1].y, 2)
+            ) / Math.sqrt(Math.pow(
+                s[0].x - s[1].x, 2) + Math.pow(s[0].y - s[1].y, 2)
+            )).toFixed(2);
+    }
+
+    function rotation(s, e) {
+        return ((Math.atan2(e[0].y - e[1].y, e[0].x - e[1].x) * 180 / Math.PI) -
+                (Math.atan2(s[0].y - s[1].y, s[0].x - s[1].x) * 180 / Math.PI)
+            ).toFixed(2);
+    }
+
     var THRESHOLD = 0.25; //cm
 
     var active = {};
@@ -25,17 +39,13 @@
                 // lock the 2 pointers to the target
                 // they may move outside of the target later on
                 active[p[0].id] = {
-                    start: p[0].events.move ?
-                        p[0].events.move[p[0].events.move.length-1] :
-                        p[0].events.down[0],
+                    start: p[0].position(),
                     related: p[1].id,
                     target: eventData.target,
                     hasStarted: false
                 };
                 active[p[1].id] = {
-                    start: p[1].events.move ?
-                        p[1].events.move[p[1].events.move.length-1] :
-                        p[1].events.down[0],
+                    start: p[1].position(),
                     related: p[0].id,
                     target: eventData.target,
                     hasStarted: false
@@ -45,7 +55,7 @@
 
         move: function (pointer, eventData, originalEvent) {
             var cur = active[pointer.id],
-                rel, props;
+                rel, relPointer, relPosition, props = {};
 
             if (cur) {
                 rel = active[cur.related];
@@ -63,8 +73,14 @@
                     }
                 }
 
+                relPointer = global.gesture.pointer.get(cur.related);
+                relPosition = relPointer.position();
+
+                props.scale = scale([cur.start, rel.start], [eventData, relPosition]);
+                props.rotation = rotation([cur.start, rel.start], [eventData, relPosition]);
+
                 // todo calc
-                global.gesture.trigger(cur.target, 'pinch', {});
+                global.gesture.trigger(cur.target, 'pinch', props);
             }
         },
 
@@ -82,8 +98,6 @@
 
                 delete active[cur.related];
                 delete active[pointer.id];
-
-
             }
         }
     };
